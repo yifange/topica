e164f35e1979263f69f63de4171d4c27e06820ee
yifange <irfan.1989@gmail.com>
2013-10-15 19:34:24 -0400

    added backend

---
+++ atrium/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Atrium::Application.config.secret_key_base = '242a4046342ccad1424845cb3a048fd812466a69d9272c7bc8b3231c8c5909e0faf69dd33229ad8c0b6cc9dd0e08954c57a089a1ae70dddd247bc58ce920f72d'
