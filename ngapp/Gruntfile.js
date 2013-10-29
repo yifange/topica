05e6d76d0a3a8a467398fb480a57c0be68ea42fe
yifange <irfan.1989@gmail.com>
2013-10-28 23:03:56 -0400

    proxy support

--- ngapp/Gruntfile.js
+++ ngapp/Gruntfile.js
@@ -5,6 +5,7 @@ var lrSnippet = require('connect-livereload')({ port: LIVERELOAD_PORT });
 var mountFolder = function (connect, dir) {
   return connect.static(require('path').resolve(dir));
 };
+var proxySnippet = require('grunt-connect-proxy/lib/utils').proxyRequest;
 
 // # Globbing
 // for performance reasons we're only matching one level down:
@@ -16,6 +17,8 @@ module.exports = function (grunt) {
   require('load-grunt-tasks')(grunt);
   require('time-grunt')(grunt);
 
+  // load the proxy.
+  grunt.loadNpmTasks('grunt-connect-proxy');
   // configurable paths
   var yeomanConfig = {
     app: 'app',
@@ -70,6 +73,13 @@ module.exports = function (grunt) {
         // Change this to '0.0.0.0' to access the server from outside.
         hostname: 'localhost'
       },
+      proxies: [
+        {
+          context: '/api',
+          host: 'localhost',
+          port: 3000
+        }
+      ],
       livereload: {
         options: {
           middleware: function (connect) {
