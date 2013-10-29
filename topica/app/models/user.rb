816674d681c388f337380e5dfcff42fd035d4cd5
yifange <irfan.1989@gmail.com>
2013-10-28 23:07:44 -0400

    comment out some incorrect code

--- topica/app/models/user.rb
+++ topica/app/models/user.rb
@@ -1,10 +1,10 @@
 class User < ActiveRecord::Base
   has_many :topics
-  has_many :topics, :through => :followships
+  # has_many :topics, :through => :followships
   has_many :followships
   has_many :favors
   has_many :posts
-  has_many :posts, :through => :favors
+  # has_many :posts, :through => :favors
   has_many :feeds
   has_many :comments
   def self.following_topic
