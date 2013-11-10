70c7edbf9ea732eaa9e1549d9bd077a105e9f07d
yifange <irfan.1989@gmail.com>
2013-11-10 15:53:50 -0500

    added topic id into post

--- topica/db/migrate/20131018003716_create_posts.rb
+++ topica/db/migrate/20131018003716_create_posts.rb
@@ -2,6 +2,7 @@ class CreatePosts < ActiveRecord::Migration
   def change
     create_table :posts do |t|
       t.references :user, index: true
+      t.references :topic, index: true
       t.text :title
       t.text :content
 
