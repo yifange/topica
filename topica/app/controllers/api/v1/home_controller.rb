efef152aa0a69e3ff2b7af3f0d943696ab24c6c5
yifange <irfan.1989@gmail.com>
2013-11-30 21:44:24 -0500

    comment number

--- topica/app/controllers/api/v1/home_controller.rb
+++ topica/app/controllers/api/v1/home_controller.rb
@@ -1,5 +1,9 @@
 class Api::V1::HomeController < Api::V1::ApplicationController
   def index
-    render :json => User.find(current_user.id).posts.order(:created_at => :desc).includes(:user), :include => :user
+    posts = User.find(current_user.id).posts.order(:created_at => :desc).includes(:user, :comments)
+    posts.each do |post|
+      post.num_of_comments = post.get_num_of_comments
+    end
+    render :json => posts, :include => {:user => {:only => [:email, :id, :username]}}
   end
 end
--- topica/app/models/post.rb
+++ topica/app/models/post.rb
@@ -4,9 +4,16 @@ class Post < ActiveRecord::Base
   has_many :users, :through => :favors
   has_many :comments, :dependent => :destroy
   has_many :topics, :through => :categories
-
+  attr_accessor :num_of_comments
   def favoring_users
     user_ids = Favor.where(:post_id => id).map &:user_id
     users = User.where(:id => user_ids).select(:id, :email, :username)
+  end
+  
+  def get_num_of_comments
+    comments.count
+  end
+  def attributes
+    {:id => nil, :user_id => nil, :topic_id => nil, :title => nil, :content => nil, :created_at => nil, :updated_at => nil, :num_of_comments => nil}
   end
 end
