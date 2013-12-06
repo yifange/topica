000886d53f5d41b441e29b38e2b4219c9a3ddc8b
yifange <irfan.1989@gmail.com>
2013-12-06 16:44:51 -0500

    comment size

--- topica/app/models/post.rb
+++ topica/app/models/post.rb
@@ -4,6 +4,10 @@ class Post < ActiveRecord::Base
   has_many :categories, :dependent => :destroy
   has_many :topics, :through => :categories
   has_many :favoring_users, -> { select "users.id", :email, :username, :name}, :class_name => "User", :through => :favors, :source => :post
+  def comment_size
+    comments.size
+  end
+
   def user
     topics.first.user.select(:id, :email, :username, :name)
   end
