f5961cd68434e98469a95d371308dcb7261ddc22
Fenghuan Lu <lufenghuan@gmail.com>
2013-12-06 18:44:26 +0000

    fix rspec accoding to model change

--- topica/app/models/followship.rb
+++ topica/app/models/followship.rb
@@ -6,5 +6,4 @@ class Followship < ActiveRecord::Base
   validates_uniqueness_of :user_id, :scope => :topic_id
   validates :user, :presence => true
   validates :topic, :presence => true
-  validates :feed, :presence => true
 end
