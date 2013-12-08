52f949fd1065d26de652f846541ebc7a90271cd1
yifange <irfan.1989@gmail.com>
2013-12-07 21:09:17 -0500

    clean up

--- topica/app/controllers/api/v1/home_controller.rb
+++ topica/app/controllers/api/v1/home_controller.rb
@@ -1,10 +1,5 @@
 class Api::V1::HomeController < Api::V1::ApplicationController
   def index
-    # posts = User.find(current_user.id).posts.order(:created_at => :desc).includes(:user, :comments)
-    # posts.each do |post|
-    #   post.num_of_comments = post.get_num_of_comments
-    # end
-    # render :json => posts, :include => {:user => {:only => [:email, :id, :username]}}
     topics = User.find(current_user.id).following_topics.includes :user, :posts => :comments
     render :json => topics, :include => {:user => {}, :posts => {:methods => :comment_size}}
   end
