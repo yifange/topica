c3085442cca3d61cceea77246e2b912554d967b9
Fenghuan Lu <lufenghuan@gmail.com>
2013-12-08 20:10:10 +0000

    add spec helper

--- topica/spec/factories/comment.rb
+++ topica/spec/factories/comment.rb
@@ -2,6 +2,6 @@ FactoryGirl.define do
   factory :comment  do
     post_id           0
     user_id           0
-    content           {(0...100).map { (65 + rand(26)).chr }.join}
+    content           {MySpecUtils.random_string 100} 
   end
 end
