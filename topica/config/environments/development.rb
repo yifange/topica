2b699cc92fd8d9af2ca4676fded5a04cc57ee5cd
Fenghuan Lu <lufenghuan@gmail.com>
2013-12-10 23:55:05 +0000

    Add sunspot_match

--- topica/config/environments/development.rb
+++ topica/config/environments/development.rb
@@ -26,4 +26,6 @@ Topica::Application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
+
+  config.colorize_logging = false
 end
