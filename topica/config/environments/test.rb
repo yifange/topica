a5c7740e18160f1387dbf58b705eae0f61022c4b
Fenghuan Lu <lufenghuan@gmail.com>
2013-12-11 00:18:05 +0000

    disbale log output to console

--- topica/config/environments/test.rb
+++ topica/config/environments/test.rb
@@ -33,4 +33,7 @@ Topica::Application.configure do
 
   # Print deprecation notices to the stderr.
   config.active_support.deprecation = :stderr
+
+  # log color
+  config.colorize_logging = false
 end
